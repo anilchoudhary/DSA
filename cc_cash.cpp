86b505e6075fe555f6337b01b02e446a81574257
anilchoudhary <masteranilchoudhary@gmail.com>
2020-02-07 23:01:15 +0530

    added max_sum_subArray_partialSum

---
+++ cc_cash.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		int n,k,r=0;
+		cin>>n>>k;
+		int a[n];
+		for(int i = 0;i < n;i++)
+		{
+			cin>>a[i];
+		}
+		for(int i = 0;i < n;i++)
+		{
+			r += a[i]%k;
+			
+		}
+		
+		cout<<r%k<<endl;
+	}
+}
---
+++ cc_snug_fit.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--){
+		int n; int sum = 0;
+		cin>>n;
+		int a[n],b[n];
+		for(int i = 0;i < n;i++)
+		cin>>a[i];
+		for(int i = 0;i < n;i++)
+		cin>>b[i];
+		sort(a,a+n);
+		sort(b,b+n);
+		for(int i = 0;i < n;i++)
+		{
+			if(a[i]>b[i])
+				sum += b[i];
+			else
+				sum += a[i];
+		}
+		cout<<sum<<endl;
+	}
+}
