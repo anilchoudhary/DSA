504ee0a84238dcd6d98752f7bcf2ccc31d957a11
anilchoudhary <masteranilchoudhary@gmail.com>
2020-03-07 22:48:23 +0530

    edited cc_engxor

--- CC/cc_engxor.cpp
+++ CC/cc_engxor.cpp
@@ -23,20 +23,13 @@ int main(){
 		int  arr2[q];
 		for(int i=0;i<q;i++)
 		{
-			cin>>arr2[i];			
-		}
-		
-		
-		
-		
-		for(int i=0;i<q;i++)
-		{
+			cin>>arr2[i];
 			count=__builtin_popcount(arr2[i]);
 			if(count%2==0)
 				cout<<even<<" "<<n-even<<"\n";
 			else
 				cout<<n-even<<" "<<even<<"\n";
-			count = 0;
+			count = 0;			
 		}
 	}
 	return 0;
