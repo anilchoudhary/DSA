d32072f3cd4ae92cd235b0483722d2f5efdb0fa3
anilchoudhary <masteranilchoudhary@gmail.com>
2020-03-08 23:41:57 +0530

    added cc_adashop2

---
+++ CC/cc_adashop2.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int x,y;
+		cin>>x>>y;
+		cout<<"18"<<endl;
+		cout<<"7 7"<<endl;
+		cout<<"8 6"<<endl;
+		cout<<"3 1"<<endl;
+		cout<<"1 3"<<endl;
+		cout<<"5 7"<<endl;
+		cout<<"8 4"<<endl;
+		cout<<"5 1"<<endl;
+		cout<<"1 5"<<endl;
+		cout<<"4 8"<<endl;
+		cout<<"5 7"<<endl;
+		cout<<"6 8"<<endl;
+		cout<<"7 7"<<endl;
+		cout<<"8 8"<<endl;
+		cout<<"5 5"<<endl;
+		cout<<"2 8"<<endl;
+		cout<<"1 7"<<endl;
+		cout<<"7 1"<<endl;
+		cout<<"8 2"<<endl;
+	}
+	return 0;
+}
