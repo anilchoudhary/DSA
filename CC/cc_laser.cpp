b4b1cb48408d3f2e16147d2accbd74540574fc4a
anilchoudhary <masteranilchoudhary@gmail.com>
2020-03-15 20:03:40 +0530

    updated cc_laser

--- CC/cc_laser.cpp
+++ CC/cc_laser.cpp
@@ -3,70 +3,43 @@ using namespace std;
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    
     int t;
     cin>>t;
     while(t--)
-	{
-	    int n,q,x1,x2,y;
-	    cin>>n>>q;
-	    int arr[n],px[n],py[n],ls[n-1][2],le[n-1][2];
-	    int k = 0, l = 0;
-	    for (int i=0;i<n;i++)
-	    {
-	        cin>>arr[i];
-	        px[i] = i+1;
-	        py[i] = arr[i];
-	        if(i != 0 && i != n-1)
-			{
-		        le[l][0] = px[i];
-		        le[l][1] = py[i];
-		        l++;
-		        ls[k][0] = px[i];
-		        ls[k][1] = py[i];
-		        k++;
-	        }
-	        else if(i == 0)
-			{
-		        ls[k][0] = px[i];
-		        ls[k][1] = py[i];
-		        k++;
-	        }
-	        else if(i == n-1)
-			{
-		        le[l][0] = px[i];
-		        le[l][1] = py[i];
-		        l++;        
-			}
-	    }
-    
-	    for(int j=0;j<q;j++)
-		{
-		    cin>>x1>>x2>>y;
-		    int count = 0;
-		    float newpoint,m1,c1,c2;
-		    for(int i = 0;i<n-1;i++)
-			{
-		        if((y >= ls[i][1] && y <= le[i][1]) || (y <= ls[i][1] && y >= le[i][1]))
-				{
-		            if(x2 != ls[i][0] && x1 != le[i][0])
-					{
-						m1 = (le[i][1] - ls[i][1]) / (le[i][0] - ls[i][0]);
-						c1 = ls[i][1] - m1 * ls[i][0];
-						c2 = y;
-						newpoint = (c2 - ls[i][1])/m1 + ls[i][0];
+    {
+        int n,q,lly,lry,llx,lrx,up,down;
+        cin>>n>>q;
+        int arr[n];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
 
-			            if((x1 <= newpoint && x2 >= newpoint) || (x1 >= newpoint && x2 <= newpoint))
-						{
-			            	count++;
-			            }
-		            }
-		        }
-		    }
-		    cout<< count<<"\n";
-	    }
-	}
-return 0;
+        for(int i=0;i<q;i++)
+        {
+            int x1,x2,y,cnt=0;
+            cin>>x1>>x2>>y;
+            for(int i=x1;i<x2;i++)
+            {
+                lly=arr[i-1];
+                lry=arr[i];
+                llx=i-1;
+                lrx=i;
+                if(lly<lry)
+                {
+                    down = lly;
+                    up = lry;
+                }
+                else
+                {
+                    down = lry;
+                    up = lly;
+                }
+                if((llx != x2-1) && (lrx != x1-1) && (down <= y) && (up >= y))
+                    cnt++;
+
+            }
+            cout<<cnt<<"\n";
+        }
+    }
+    return 0;
 }
+
