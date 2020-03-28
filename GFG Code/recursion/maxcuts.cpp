f7d931ea97f876d86912a4ca6e6936032fe6580a
anilchoudhary <masteranilchoudhary@gmail.com>
2020-03-28 11:49:32 +0530

    added max cuts recursion

---
+++ GFG Code/recursion/maxcuts.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int maxcuts(int n, int a, int b,int c)
+{
+	if(n==0) return 0;
+	if(n<0) return -1;
+	int res = max(maxcuts(n-a, a, b, c), maxcuts(n-b, a, b, c));
+	res = max(res, maxcuts(n-c, a, b, c));
+	if(res==-1)
+		return -1;
+	return res+1;
+}
+
+int main()
+{
+	cout<<maxcuts(5, 4, 2, 6);
+	return 1;
+}
