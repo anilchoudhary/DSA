13d7442b89b815871ce2dca4bbb03959f9e10ac1
anilchoudhary <masteranilchoudhary@gmail.com>
2020-03-28 13:18:46 +0530

    added leadersArray

---
+++ GFG Code/array/leadersArray.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void leader (int arr[], int n)
+{
+	int max = arr[n-1];
+	for(int i = n-1; i>=0; i--)
+	{
+		if(arr[i] >= max)
+		{
+			cout<<arr[i]<<" ";
+			max = arr[i];
+		}
+	}
+}
+
+int main()
+{
+	int n = 6;
+	int arr[]= {5, 3, 20, 15, 8, 3};
+	leader(arr, n);
+	return 1;
+}
