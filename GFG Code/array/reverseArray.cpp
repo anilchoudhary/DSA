e0ce911d1feef21592b86a31f91d2226076fe6e6
anilchoudhary <masteranilchoudhary@gmail.com>
2020-03-28 11:58:08 +0530

    added reverseArray

---
+++ GFG Code/array/reverseArray.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void reverse(int arr[], int n)
+{
+	int i=0,j=n-1; int temp;
+	while(i<j)
+	{
+		temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+		++i; --j;
+	}
+}
+
+int main()
+{
+	int n = 9;
+	int arr[]= {1,2,3,4,5,6,7,8,9};
+	reverse(arr,n);
+	for(int i=0;i<n;i++)
+	{
+		cout<<arr[i]<<" ";
+	}
+	return 1;
+}
