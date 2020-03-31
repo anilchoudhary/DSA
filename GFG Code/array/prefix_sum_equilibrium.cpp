e13540c1a4134446ad85a37877aa698c9a79a610
anilchoudhary <masteranilchoudhary@gmail.com>
2020-03-31 11:55:06 +0530

    added equilibrium

---
+++ GFG Code/array/prefix_sum_equilibrium.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int sum;
+
+bool equ(int arr[], int n)
+{
+    for(int i=0;i<n;i++)
+        sum += arr[i];
+    int lsum = 0;
+    for(int i=0;i<n;i++)
+    {
+        if(lsum == sum - arr[i])
+            return true;
+        lsum += arr[i];
+        sum -= arr[i];
+    }
+    return false;
+}
+
+int main()
+{
+    int arr[] = {4,2,2};
+    int n = 3;
+    if(equ(arr, n))
+        cout<<"YES";
+    else
+        cout<<"NO";
+    return 1;
+}
