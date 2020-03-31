40624c9d20b999f0db00aacfaa18f442d7060b9e
anilchoudhary <masteranilchoudhary@gmail.com>
2020-03-31 10:17:31 +0530

    added prefix sum 1

---
+++ GFG Code/array/prefix sum 1.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+int psum[100];
+
+void prefixsum(int arr[], int n)
+{
+	psum[0] = arr[0];
+	for(int i=1;i<n;i++)
+		psum[i] = psum[i-1] + arr[i];
+}
+
+void query(int l, int r)
+{
+	int ans;
+	if(l!=0)
+	{
+		ans = psum[r] - psum[l-1];
+	}
+	else
+		ans = psum[r];
+	cout<<ans<<endl;
+}
+
+int main()
+{
+	int arr[] = {2,8,3,9,6,5,4};
+	int n = 7;
+	prefixsum(arr, n);
+	query(0,2);
+	query(1,3);
+	query(2,6);
+	return 1;
+}
