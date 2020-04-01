0978e24c86f73d4f75e0f640fb4c9141a5a94267
anilchoudhary <masteranilchoudhary@gmail.com>
2020-04-01 12:24:55 +0530

    added nonRepeatingCharacter

---
+++ GFG Code/array/nonRepeatingCharacter.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+char nonRepeatingCharacter(string str)
+{
+    map<char,int> frequency;
+
+    for(int i = 0; i<str.length(); i++)
+    {
+        char currentChar = str[i];
+        frequency[currentChar]++;
+    }
+    for(int i=0;i<str.length();i++)
+    {
+        if(frequency[str[i]] == 1)
+            return str[i];
+    }
+    return str[0];
+}
+
+int main()
+{
+    string str = "adHahdbc";
+    char answer = nonRepeatingCharacter(str);
+    cout<<answer;
+    return 1;
+}
