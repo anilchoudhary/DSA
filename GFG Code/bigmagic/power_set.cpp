554713d4385edbc2219b704f27a0d176c8466973
anilchoudhary <masteranilchoudhary@gmail.com>
2020-04-01 23:39:31 +0530

    added power_set

---
+++ GFG Code/bigmagic/power_set.cpp
@@ -0,0 +1,18 @@
+// Generate power set
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string str = "abc";
+    int n = str.length();
+    int count = (1 << n);
+    for(int i = 0; i < count; i++)
+    {
+        for(int j=0; j<n; j++)
+            if((i & (1<<j)))
+                cout<<(str[j]);
+        cout<<endl;
+    }
+}
