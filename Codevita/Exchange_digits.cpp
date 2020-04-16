045782ea7ff4d9f8996258d79cc3a08030b0647b
anilchoudhary <masteranilchoudhary@gmail.com>
2020-04-16 11:48:05 +0530

    added cycle sort

---
+++ Codevita/Exchange_digits.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    long long int a,b;
+    cin>>a>>b;
+    vector<int> arr,brr;
+
+
+    while(a>0)
+    {
+        arr[i] = a%10;
+        a = a/10;
+    }
+    while(b>0)
+    {
+        brr[i] = b%10;
+        b = b/10;
+    }
+    int an = arr.size();
+    int bn = brr.size();
+
+
+
+
+}
---
+++ Codevita/clock_angle.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Utility function to find minimum of two integers
+int min(int x, int y)
+{
+    return (x < y)? x: y;
+
+}
+
+float calcAngle(double h, double m)
+{
+    // validate the input
+    if (h <0 || m < 0 || h >12 || m > 60)
+       // printf("Wrong input");
+
+    if (h == 12) h = 0;
+    if (m == 60) m = 0;
+
+    // Calculate the angles moved
+    // by hour and minute hands
+    // with reference to 12:00
+    int hour_angle = 0.5 * (h * 60 + m);
+    int minute_angle = 6 * m;
+
+    // Find the difference between two angles
+    float angle = abs(hour_angle - minute_angle);
+
+    // Return the smaller angle of two possible angles
+    angle = min(360 - angle, angle);
+
+    return angle;
+}
+
+// Clock Angle Problem
+int main()
+{
+    float h;
+    float l;
+    cin>>h>>l;
+
+    float t;
+    int ti;
+    t = (h/360)*l;
+    ti = t;
+   // cout<<t<<endl;
+   // cout<<ti;
+    float m = t-ti;
+    m *= 60;
+    cout<<ti<<" " <<m<<endl;
+	cout <<fixed<<setprecision(2)<< calcAngle(ti, m);
+
+	return 0;
+}
