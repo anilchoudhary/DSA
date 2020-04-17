e5ad653fa4f9e7e13db2f0e164658475d99025a0
anilchoudhary <masteranilchoudhary@gmail.com>
2020-04-17 10:55:12 +0530

    added trailing zerores in factorials

---
+++ Codevita/trailing_zeroes_in_factorials.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int tz(int n)
+{
+    int ans = 0;
+    int p = 5;
+    while((n/p)>0)
+    {
+        ans += n/p;
+        p *= 5;
+    }
+    return ans;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<tz(n);
+    return 0;
+}
