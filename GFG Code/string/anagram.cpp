c84b56b4cb871200ec73840c390dbbb114905615
anilchoudhary <masteranilchoudhary@gmail.com>
2020-04-21 07:18:00 +0530

    added anagram

---
+++ GFG Code/string/anagram.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s1,s2;
+    s1 = "gfgf";
+    s2 = "ggfg";
+
+    int n= s1.length();
+    int m = s2.length();
+
+    if(n!=m)
+    {
+        cout<<"no"<<endl;
+        return 0;
+    }
+    int ch[256]={0};
+    for(int i=0;i<n;i++)
+    {
+        ch[s1[i]]++;
+        ch[s2[i]]--;
+    }
+    for(int i=0;i<256;i++)
+    {
+        if(ch[i]!=0)
+        {
+            cout<<"no"<<endl;
+            return 0;
+        }
+    }
+    cout<<"yes"<<endl;
+    return 0;
+}
