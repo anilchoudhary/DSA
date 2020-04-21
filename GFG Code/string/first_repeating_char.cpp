db2638e359bd3639c0de1c8b1ba884b84f0cfc4b
anilchoudhary <masteranilchoudhary@gmail.com>
2020-04-21 09:10:10 +0530

    added first repeating char

---
+++ GFG Code/string/first_repeating_char.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+#define MAX_CHAR 256
+
+int findRepeatFirst(char* s)
+{
+	int p = -1, i, k;
+
+	int hash[MAX_CHAR] = { 0 };
+
+	int pos[MAX_CHAR];
+
+	for (i = 0; i < strlen(s); i++) {
+		k = (int)s[i];
+		if (hash[k] == 0) {
+			hash[k]++;
+			pos[k] = i;
+		} else if (hash[k] == 1)
+			hash[k]++;
+	}
+
+	for (i = 0; i < MAX_CHAR; i++) {
+         if(hash[i]!=0)
+            cout<<hash[i]<<endl;
+		if (hash[i] == 2) {
+			if (p == -1)
+				p = pos[i];
+			else if (p > pos[i])
+				p = pos[i];
+		}
+	}
+
+	return p;
+}
+
+int main()
+{
+	char str[] = "geeksforgeeks";
+	int pos = findRepeatFirst(str);
+	if (pos == -1)
+		cout << "Not found";
+	else
+		cout << str[pos];
+	return 0;
+}
+
