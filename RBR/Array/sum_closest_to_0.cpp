4a1b8b158f932b422d9eaf8956c4b1d928d4f370
anilchoudhary <masteranilchoudhary@gmail.com>
2020-04-29 23:01:59 +0530

    code formatted

--- RBR/Array/sum_closest_to_0.cpp
+++ RBR/Array/sum_closest_to_0.cpp
@@ -3,31 +3,31 @@ using namespace std;
 
 int main()
 {
-    int n,l,h,mini,dif,x,y,sum=0,flag=0;
-    n=6;
+    int n, l, h, mini, dif, x, y, sum = 0, flag = 0;
+    n = 6;
     x = 19;
-    int arr[n] = {0,3,44,5,9,7};
-    sort(arr, arr+n);
-    for(int i=0;i<n-2;i++)
+    int arr[n] = {0, 3, 44, 5, 9, 7};
+    sort(arr, arr + n);
+    for (int i = 0; i < n - 2; i++)
     {
-        l = i+1;
-        h = n-1;
-        while(l<h)
+        l = i + 1;
+        h = n - 1;
+        while (l < h)
         {
             sum = arr[i] + arr[l] + arr[h];
-            if(sum < x)
+            if (sum < x)
                 l++;
-            if(sum > x)
+            if (sum > x)
                 h--;
-            if(sum == x)
+            if (sum == x)
             {
                 flag = 1;
-                cout<<arr[i]<< " "<< arr[l]<< " "<<arr[h];
+                cout << arr[i] << " " << arr[l] << " " << arr[h];
                 break;
             }
         }
     }
-    if(!flag)
-        cout<<"not found";
+    if (!flag)
+        cout << "not found";
     return 0;
 }
