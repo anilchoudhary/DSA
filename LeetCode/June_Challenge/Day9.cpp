11228ca03619dbd0356128dace32e1ccaa471d2b
anilchoudhary <masteranilchoudhary@gmail.com>
2020-06-09 22:40:23 +0530

    added is two sets disjoint

--- LeetCode/June_Challenge/Day9.cpp
+++ LeetCode/June_Challenge/Day9.cpp
@@ -9,5 +9,4 @@ public:
 		}
 		return (x == s.size());
 	}
-
 };
--- RBR/Hashing/two_set_disjoints_or_not.cpp
+++ RBR/Hashing/two_set_disjoints_or_not.cpp
@@ -3,7 +3,24 @@ using namespace std;
 
 int main()
 {
-	int arr[] = {3, 24, 5, 73, 2, 5, 6, 2, 1};
-	int n = sizeof(arr) / sizeof(arr[0]);
+	int arr[] = {99, 66, 55, 44, 88, 99, 22, 33};
+	int brr[] = {3, 24, 5, 73, 2, 5, 6, 2, 1};
+	int m = sizeof(arr) / sizeof(arr[0]);
+	int n = sizeof(brr) / sizeof(brr[0]);
+	int flag = 1;
+	unordered_set<int> isDisjoint;
+	for (int i = 0; i < m; i++)
+		isDisjoint.insert(arr[i]);
+
+	for (int i = 0; i < n; i++)
+	{
+		if (isDisjoint.find(brr[i]) != isDisjoint.end())
+			flag = 0;
+	}
+	if (flag)
+		cout << "Disjoint";
+	else
+		cout << "Not Disjoint";
+	return 0;
 
 }
