e28f1fd1c426cfaade19a33d37b1c19d444cd436
anilchoudhary <masteranilchoudhary@gmail.com>
2020-06-12 07:19:12 +0530

    added two number sum

---
+++ AE/1/two_number_sum.cpp
@@ -0,0 +1,24 @@
+#include"bits/stdc++.h"
+using namespace std;
+
+int main()
+{
+	int arr[] = { 3, 5, -4, 5, 11, 1, -1, 7};
+	int n = sizeof(arr) / sizeof(arr[0]);
+	int sum = 10;
+	int x;
+	unordered_set<int> mymap;
+
+	for (int i = 0; i < n; i++)
+	{
+		x = sum - arr[i];
+		if (mymap.find(x) == mymap.end())
+			mymap.insert(arr[i]);
+		else
+		{
+			cout << arr[i] << " " << x << endl;
+			mymap.insert(arr[i]);
+		}
+	}
+	return 0;
+}
