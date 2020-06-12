ce83dbee05ac5044c075c8800e5df0508fa6d4e4
anilchoudhary <masteranilchoudhary@gmail.com>
2020-06-12 22:56:01 +0530

    done selection sort

---
+++ AE/1/selection_sort.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void selection_sort(int a[], int n)
+{
+	int i, j, loc, temp, min;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		min = a[i];
+		loc = i;
+		for (j = i + 1; j < n; j++)
+		{
+			if (min > a[j])
+			{
+				min = a[j];
+				loc = j;
+			}
+		}
+
+		temp = a[i];
+		a[i] = a[loc];
+		a[loc] = temp;
+	}
+
+	cout << "\nSorted list is as follows\n";
+	for (i = 0; i < n; i++)
+	{
+		cout << a[i] << " ";
+	}
+}
+
+int main()
+{
+	int arr[] = { 141, 1, 17, -7, -17, -27, 18, 541, 8, 7, 7};
+	int n = sizeof(arr) / sizeof(arr[0]);
+	selection_sort(arr, n);
+}
