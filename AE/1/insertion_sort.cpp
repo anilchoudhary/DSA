e905088935271bd75afd1f667a7fae4fdb48e501
anilchoudhary <masteranilchoudhary@gmail.com>
2020-06-12 11:39:04 +0530

    added insertion sort

---
+++ AE/1/insertion_sort.cpp
@@ -0,0 +1,31 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+void insertion_sort(int arr[], int n)
+{
+	int j = 0, temp;
+	for (int i = 1; i < n; i++)
+	{
+		temp = arr[i];
+		j = i - 1;
+		while (j >= 0 && temp < arr[j])
+		{
+			arr[j + 1] = arr[j];
+			j--;
+		}
+		arr[j + 1] = temp;
+
+	}
+
+	for (int i = 0; i < n; i++)
+		cout << arr[i] << " ";
+}
+
+int main()
+{
+	int arr[] = { 141, 1, 17, -7, -17, -27, 18, 541, 8, 7, 7};
+	int n = sizeof(arr) / sizeof(arr[0]);
+	insertion_sort(arr, n);
+
+	return 0;
+}
