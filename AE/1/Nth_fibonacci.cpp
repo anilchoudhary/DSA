af1c2e73f305f94ab4fcc9737671dc3ca6e4fe78
anilchoudhary <masteranilchoudhary@gmail.com>
2020-06-14 20:54:05 +0530

    added doubly linked list

--- AE/1/Nth_fibonacci.cpp
+++ AE/1/Nth_fibonacci.cpp
@@ -1,11 +1,24 @@
 #include "bits/stdc++.h"
 using namespace std;
 
+// Time Complexity = O(n)
+// Space Complexity = O(1)
+
 int main()
 {
 	int n = 6;
 	int i, j, k, temp;
 	i = 0; j = 1;
+	if (n == 1)
+	{
+		cout << 0 << endl;
+		return 0;
+	}
+	else if (n == 2)
+	{
+		cout << 1 << endl;
+		return 0;
+	}
 	for (int l = 0; l < n - 2; l++)
 	{
 		k = i + j;
