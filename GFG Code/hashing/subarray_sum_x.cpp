0a864ce3c51480a094a47b3669e15b046fe177d2
anilchoudhary <masteranilchoudhary@gmail.com>
2020-06-23 11:53:37 +0530

    done subarray sum X

---
+++ GFG Code/hashing/subarray_sum_x.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n = 6;
+    int x = 21;
+    int arr[n] = {5, 1, -4, 3, 8, 10};
+    int presum = 0;
+    unordered_set <int> us;
+    us.insert(0);
+    for (int i = 0; i < n; i++)
+    {
+        presum += arr[i];
+        if (us.find(presum - x) != us.end())
+        {
+            cout << "yes" << endl;
+            return 0;
+        }
+        us.insert(presum);
+    }
+    cout << "no" << endl;
+
+    return 0;
+}
