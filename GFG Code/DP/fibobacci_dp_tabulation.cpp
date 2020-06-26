1d078f1f4ca47bfcdc63b7dfde32094ed3cf7e9c
anilchoudhary <masteranilchoudhary@gmail.com>
2020-06-26 12:59:53 +0530

    done fibonacci tabulation

--- GFG Code/DP/fibobacci_dp_tabulation.cpp
+++ GFG Code/DP/fibobacci_dp_tabulation.cpp
@@ -4,16 +4,12 @@ using namespace std;
 int memo[1000] = {0};
 int fib(int n)
 {
-	if (memo[n] != 0)
-		return memo[n];
-
-	if (n <= 1)
+	memo[0] = 0;
+	memo[1] = 1;
+	for (int i = 2; i <= n; i++)
 	{
-		memo[n] = n;
-		return memo[n];
+		memo[i] = memo[i - 1] + memo[i - 2];
 	}
-	memo[n] = fib(n - 1) + fib(n - 2);
-
 	return memo[n];
 }
 
