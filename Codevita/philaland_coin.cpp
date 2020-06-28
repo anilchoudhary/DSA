cba8830fb5faece9745e5238ddea7065ab71849a
anilchoudhary <masteranilchoudhary@gmail.com>
2020-06-28 07:55:51 +0530

    done philaland coin

---
+++ Codevita/philaland_coin.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int n;
+		cin >> n;
+		int x = 0;
+		while (pow(2, x) <= n)
+			x++;
+		cout << x << endl;
+	}
+}
