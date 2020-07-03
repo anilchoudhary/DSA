a1776a420a43989ed128fda19798766f070ae8e2
anilchoudhary <masteranilchoudhary@gmail.com>
2020-07-03 21:26:46 +0530

    done maximum rope pieces

--- GFG__2/Recursion/maximum_rope_pieces.cpp
+++ GFG__2/Recursion/maximum_rope_pieces.cpp
@@ -1,20 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int cut_rope(int n, int arr[])
+int cut_rope(int n, int a, int b, int c)
 {
 
-	if (n <= 0)
+	if (n == 0)
 		return 0;
-	else if (n % *min_element(arr, arr + 3) != 0)
+	else if (n < 0)
 		return -1;
-	n -= *min_element(arr, arr + 3);
-	return 1 + cut_rope(n, arr);
+	int l = cut_rope(n - a, a, b, c);
+	int m = cut_rope(n - b, a, b, c);
+	int k = cut_rope(n - c, a, b, c);
+
+	int ans = max(max(l, m), k);
+	if (ans == -1) return -1;
+	return 1 + ans;
 }
 
 int main()
 {
 	int rope_length = 10;
-	int pieces_length[] = {12, 9, 11};
-	cout << cut_rope(rope_length, pieces_length) << endl;
+	int a = 7, b = 7, c = 4;
+	cout << cut_rope(rope_length, a, b, c) << endl;
 }
