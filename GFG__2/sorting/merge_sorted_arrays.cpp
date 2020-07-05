ca2f7a05ddc909a61b3f009925680ad07be2b549
anilchoudhary <masteranilchoudhary@gmail.com>
2020-07-06 00:10:40 +0530

    done merge sorted arrays

--- GFG__2/sorting/merge_sorted_arrays.cpp
+++ GFG__2/sorting/merge_sorted_arrays.cpp
@@ -4,21 +4,36 @@ using namespace std;
 void merge(int arr[], int brr[], int n, int m)
 {
 	int i = 0, j = 0;
-	int a[n + m];
-	for (int k = 0; k <= m + n; k++)
+	int a[n + m ];
+	int k = 0;
+	while (i < n && j < m)
 	{
-
 		if (arr[i] > brr[j])
 		{
-			a[k] = brr[j];
+			a[k++] = brr[j];
 			j++;
 		}
 		else
 		{
-			a[k] = arr[i];
+			a[k++] = arr[i];
 			i++;
 		}
 	}
+
+
+
+
+	while (i < n)
+	{
+		a[k++] = arr[i++];
+	}
+
+
+	while (j < m)
+	{
+		a[k++] = brr[j++];
+	}
+
 
 	for (auto x : a)
 	{
@@ -28,8 +43,8 @@ void merge(int arr[], int brr[], int n, int m)
 
 int main()
 {
-	int arr[]  = {3, 4, 5, 7, 8, 2, 1, 6, 7, 75, 63, 2, 1, 34, 76, 54, 31, 65, 535, 34, 6, 75, 3, 432, 53, 725, 4};
-	int brr[] = {3, 2, 6, 8, 3, 1, 34, 87, 45, 67, 143, 54, 14, 87, 14, 76, 142, 65, 243, 67, 31};
+	int arr[]  = {7, 63, 2, 1, 35};
+	int brr[] = {8, 3, 34, 87, 14};
 	int n = sizeof(arr) / sizeof(arr[0]);
 	int m = sizeof(brr) / sizeof(brr[0]);
 	sort(arr, arr + n);
