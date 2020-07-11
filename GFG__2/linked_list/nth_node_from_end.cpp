8d150650967ad3e60d5d5f3d291531ca4dfaed8c
anilchoudhary <masteranilchoudhary@gmail.com>
2020-07-11 20:16:52 +0530

    done nth node from end

---
+++ GFG__2/linked_list/nth_node_from_end.cpp
@@ -0,0 +1,162 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct node
+{
+	int data;
+	node* next;
+	node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+node* insert_end(node *head, int d)
+{
+	node* temp = new node(d);
+
+	if (head == NULL)
+		return temp;
+	node* cur = head;
+	while (cur->next)
+	{
+		cur = cur->next;
+	}
+	cur->next = temp;
+	return head;
+}
+
+void print(node *head)
+{
+	node *cur = head;
+	while (cur)
+	{
+		cout << cur->data << " ";
+		cur = cur->next;
+	}
+	cout << endl;
+}
+
+node* delete_begin(node *head)
+{
+	if (!head)
+		return NULL;
+
+	node* temp = head->next;
+	free(head);
+
+	return temp;
+}
+
+node* delete_end(node *head)
+{
+	if (!head)
+		return NULL;
+	if (head->next == NULL)
+	{
+		free(head);
+		return NULL;
+	}
+
+	node * cur = head;
+	while (cur->next->next)
+	{
+		cur = cur->next;
+	}
+	node* temp = cur->next;
+	cur->next = NULL;
+	free(temp);
+	return head;
+}
+
+node* insert_at_pos(node* head, int i, int d)
+{
+	node* cur = head;
+	node* temp = new node(d);
+	if (i == 1)
+	{
+		temp->next = head;
+		head = temp;
+		return head;
+	}
+	--i;
+	while (--i && cur)
+	{
+		cur = cur->next;
+	}
+	if (!cur)
+		return head;
+	temp->next = cur->next;
+	cur->next = temp;
+	return head;
+
+}
+
+node* sorted_insert(node* head, int d)
+{
+	node* temp = new node(d);
+	if (head == NULL)
+		return temp;
+	if (d < head->data)
+	{
+		temp-> next = head;
+		return temp;
+	}
+	node *cur = head;
+	while (cur->next && d > cur->next->data)
+	{
+		cur = cur->next;
+	}
+	temp->next = cur->next;
+	cur->next = temp;
+	return head;
+}
+
+int middle(node*head)
+{
+	node* hare = head;
+	node* tortoise = head;
+
+	while (hare && hare->next)
+	{
+		hare = hare->next->next;
+		tortoise = tortoise->next;
+	}
+	return tortoise->data;
+}
+
+int nth_node_end(node *head, int n)
+{
+	node *slow = head;
+	node *fast = head;
+
+	while (n--)
+	{
+		fast = fast -> next;
+	}
+	while (fast)
+	{
+		slow = slow -> next;
+		fast = fast -> next;
+	}
+	return slow->data;
+}
+
+int main()
+{
+	node* head = NULL;
+	head = insert_end( head , 3);
+	insert_end( head , 5);
+	insert_end( head , 6);
+	insert_end( head , 7);
+	head = insert_at_pos(head, 1, 2);
+	// print(head);
+	head = sorted_insert(head, 4);
+	print(head);
+	cout << nth_node_end(head, 4) << endl;
+	// cout << middle(head) << endl;
+	return 0;
+}
+
+
