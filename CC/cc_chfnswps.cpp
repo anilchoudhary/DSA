423826b35fd0006154d8143b7d28ffb35c2771ff
anilchoudhary <masteranilchoudhary@gmail.com>
2020-07-12 23:59:09 +0530

    added cc_chfnswps

---
+++ CC/cc_chfnswps.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int n;
+		cin >> n;
+		int arr[n], brr[n];
+		for (int i = 0; i < n; i++)
+		{
+			cin >> arr[i];
+		}
+		for (int i = 0; i < n; i++)
+		{
+			cin >> brr[i];
+		}
+		sort(arr, arr + n, greater<int>);
+		sort(brr, brr + n, greater<int>);
+	}
+}
