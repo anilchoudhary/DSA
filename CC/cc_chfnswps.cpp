af8dccf2e800c498123b3478bc3451692bab563e
anilchoudhary <masteranilchoudhary@gmail.com>
2020-07-13 20:36:31 +0530

    done chfnswps

--- CC/cc_chfnswps.cpp
+++ CC/cc_chfnswps.cpp
@@ -1,7 +1,11 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
+#define int long long
+#define endl "\n"
+
 using namespace std;
 
-int main()
+int32_t main()
 {
 #ifndef ONLINE_JUDGE
 	freopen("input.txt", "r", stdin);
@@ -9,21 +13,77 @@ int main()
 #endif
 	int t;
 	cin >> t;
+
 	while (t--)
 	{
 		int n;
 		cin >> n;
-		int arr[n], brr[n];
-		for (int i = 0; i < n; i++)
-		{
-			cin >> arr[i];
-		}
-		for (int i = 0; i < n; i++)
-		{
-			cin >> brr[i];
-		}
-		sort(arr, arr + n, greater<int>);
-		sort(brr, brr + n, greater<int>);
 
+		vector <int> arr, brr;
+		int x;
+		map <int, int> mymap;
+
+		for (int i = 0; i < n; i++)
+		{
+			cin >> x;
+
+			mymap[x]++;
+			// 	arr.push_back(x);
+		}
+
+		for (int i = 0; i < n; i++)
+		{
+			cin >> x;
+
+			mymap[x]--;
+			// 	brr.push_back(x);
+		}
+
+		bool flag = false;
+		// arr.clear();
+		// brr.clear();
+
+		int mi = x;
+
+		for (auto i : mymap)
+		{
+			mi = min(mi, i.first);
+			x = abs(i.second);
+
+			if (x % 2)
+				flag = true;
+
+			x = i.second;
+
+			if (x > 0)
+			{
+				x /= 2;
+
+				while (x--)
+					arr.push_back(i.first);
+			}
+			else if (x < 0)
+			{
+				x = abs(x) / 2;
+
+				while (x--)
+					brr.push_back(i.first);
+			}
+		}
+
+		if (flag)
+		{
+			cout << -1 << endl;
+			continue;
+		}
+
+		reverse(brr.begin(), brr.end());
+
+		int ans = 0;
+
+		for (int i = 0; i < arr.size(); i++)
+			ans += min(2 * mi, min(arr[i], brr[i]));
+
+		cout << ans << endl;
 	}
 }
