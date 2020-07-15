1a50535b4ae6c299d697a76bafd977d5a61372b4
anilchoudhary <masteranilchoudhary@gmail.com>
2020-07-15 19:58:46 +0530

    done total nodes

---
+++ GFG__2/tree/total_nodes.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct node
+{
+	int data;
+	node *left;
+	node *right;
+	node(int d)
+	{
+		this->data = d;
+		left = NULL;
+		right = NULL;
+	}
+};
+
+int total(node *root)
+{
+	if (root == NULL)
+		return 0;
+	else
+		return (1 + total(root->left) + total(root -> right));
+}
+
+int main()
+{
+	node *root = new node(10);
+	node *left = new node(20);
+	node *right = new node(30);
+	root -> left = left;
+	root -> right = right;
+	cout << total(root) << endl;;
+	return 0;
+
+}
