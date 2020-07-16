627672e6d64c85808f51703ad2f5b0fc33317fae
anilchoudhary <masteranilchoudhary@gmail.com>
2020-07-16 18:03:20 +0530

    done search in bst

---
+++ GFG__2/BST/search.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct node
+{
+	int data;
+	node *left;
+	node *right;
+	node(int d)
+	{
+		this->data = d;
+		left = NULL;
+		right = NULL;
+	}
+};
+
+void iterative_inorder(node *root)
+{
+	if (root == NULL) return ;
+	stack<node *> st;
+	st.push(root);
+	while (st.empty() == false)
+	{
+		node *cur = st.top();
+		cout << cur->data << " ";
+		st.pop();
+		if (cur -> right != NULL) st.push(cur -> right);
+		if (cur -> left != NULL) st.push(cur -> left);
+	}
+}
+
+bool search(node *root , int x)
+{
+	if (root == NULL)
+		return 0;
+	else if (root -> data == x)
+		return 1;
+	else if (root -> data > x)
+		return search(root -> left, x);
+	else
+		return search(root -> right, x);
+}
+
+int main()
+{
+	node *root = new node(20);
+	node *left = new node(10);
+	node *right = new node(30);
+	root -> left = left;
+	root -> right = right;
+	if (search(root, 340))
+		cout << "found" << endl;
+	else
+		cout << "not found" << endl;
+	// iterative_inorder(root);
+	return 0;
+
+}
