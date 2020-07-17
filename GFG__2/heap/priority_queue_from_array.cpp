034ba638c752d30ecb98fe6308dab481a0e93cca
anilchoudhary <masteranilchoudhary@gmail.com>
2020-07-17 22:49:28 +0530

    done day 17 july leetcode

--- GFG__2/heap/priority_queue_from_array.cpp
+++ GFG__2/heap/priority_queue_from_array.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
 	int arr[] = {5, 10, 15};
-	priority_queue<int, vector<int> , greater<int>> pq(arr, arr + 3);
+	priority_queue<int, vector<int> , greater<int> > pq(arr, arr + 3);
 	cout << pq.size() << endl;
 	cout << pq.top() << endl;
 	while (pq.empty() == false)
