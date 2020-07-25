8ea9659d7578501e0a48546e3efb373d88efaaca
anilchoudhary <masteranilchoudhary@gmail.com>
2020-07-25 23:37:01 +0530

    done divine divisors approach 2

--- Codevita/CQ/divine_divisors_approach_2.cpp
+++ Codevita/CQ/divine_divisors_approach_2.cpp
@@ -6,10 +6,11 @@ int32_t main()
 	int n;
 	// cin >> n;
 	n = 10;
-	for (int i = 1; i <= n; i++)
+	for (int i = 1; i <= n / 2; i++)
 	{
 		if (n % i == 0)
 			cout << i << " ";
 	}
+	cout << n << endl;
 	return 0;
 }
