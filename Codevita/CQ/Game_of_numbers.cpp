9e662a09931fe0809983bf0006ea331048ab2893
anilchoudhary <masteranilchoudhary@gmail.com>
2020-08-03 22:39:36 +0530

    done day 3 august leetcode

---
+++ Codevita/CQ/Game_of_numbers.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen ("output.txt", "w", stdout);
+#endif
+	int n;
+	cin >> n;
+	int *arr = new int[n];
+	int ans = 0;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+	for (int i = 0; i < n; i++)
+		ans ^=  arr[i];
+	if (ans)
+		cout << "AMAN" << endl;
+	else
+		cout << "JASBIR" << endl;
+	return 0;
+
+}
