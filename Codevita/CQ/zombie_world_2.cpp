83cd7564278d8285b09b2c6bfaee949333a37991
anilchoudhary <masteranilchoudhary@gmail.com>
2020-08-03 07:37:10 +0530

    done zombie world 2

---
+++ Codevita/CQ/zombie_world_2.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	int b, n;
+	cin >> b >> n;
+	int zom_energy[n];
+	for (int i = 0; i < n; i++)
+	{
+		cin >> zom_energy[i];
+	}
+	// sort(zom_energy, zom_energy + n);
+	for (int i = 0; i < n; i++)
+	{
+		if (b >= zom_energy[i])
+		{
+			b = b - ((zom_energy[i] % 2) + (zom_energy[i] / 2));
+		}
+		else
+		{
+			cout << "NO" << endl;
+			return 0;
+		}
+	}
+	cout << "YES" << endl;
+	return 0;
+}
