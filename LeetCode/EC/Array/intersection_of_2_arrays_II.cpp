ff422b2f8bccfb1c8a992ec994602dda955c227f
anilchoudhary <masteranilchoudhary@gmail.com>
2020-08-05 21:33:35 +0530

    done intersection of 2 arrays II

---
+++ LeetCode/EC/Array/intersection_of_2_arrays_II.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> nums3;
+        for (int i = 0; i < nums1.size(); i++)
+        {
+            for (int j = 0; j < nums2.size(); j++)
+            {
+                if (nums1[i] == nums2[j])
+                {
+                    nums3.push_back(nums1[i]);
+                    nums2[j] = -99999;
+                    break;
+                }
+            }
+        }
+        return nums3;
+    }
+};
